a29aa01a0ef1dbda82cd45fd888fdabeecb7d7d8
Fernando <jenoxmc@gmail.com>
2020-06-22 23:23:21 -0300

    cambio en base de datos

--- src/components/Main.js
+++ src/components/Main.js
@@ -16,16 +16,26 @@ export default class Main extends React.Component{
 							<div>Cargando...</div>
 						:
 							<>
+								<p className="text-black" style={{paddingTop: '1.5rem'}}>Ya disponible</p>	
 								<div className="products-container">
-										{/* <p className="text-black" style={{paddingTop: '1.5rem'}}>Ya disponible</p>	 */}
 									<Row>
-									{/* xs={{span: 24}} lg={{span: 6}} */}
-										{this.props.products.map(prod => (
-											<Col >
-												<ProductCard product={prod}/> 
-											</Col>
-										))}
+										{/* <Col xs={{span: 1}} lg={{span: 2}}></Col> */}
+
+											{/* <Col xs={{span: 22}} lg={{span: 24}}> */}
+												{this.props.products.map(prod => (
+													<Col xs={{span: 22}} lg={{span: 4}}>
+														<ProductCard product={prod}/> 
+													</Col>
+												))}
+											{/* </Col> */}
+										{/* <Col xs={{span: 1}} lg={{span: 2}}></Col> */}
 									</Row>
+								</div>
+								<p className="text-black" style={{paddingTop: '1.5rem'}}>Más títulos</p>
+								<div className="products-container">
+									<div className="">
+										a
+									</div>
 								</div>
 							</>
 					}
--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,15 +1,32 @@
 import React, { Fragment } from 'react';
 import {Link} from "react-router-dom";
-import { Card, Row, Col } from 'antd';
+import { Card, Row, Col, Popover, Button, Carousel } from 'antd';
 const { Meta } = Card;  
 
 export default class ProductCard extends React.Component{
 	getPhoto(prodId) {
-			return `https://firebasestorage.googleapis.com/v0/b/rolling-store-af.appspot.com/o/products%2F${prodId}.jpg?alt=media`
+		return `https://firebasestorage.googleapis.com/v0/b/rolling-store-af.appspot.com/o/products%2F${prodId}.jpg?alt=media`
 	}
 
 	render(){
-		const { name, brand, price, id } = this.props.product;
+		const { name, brand, price, id, description, date } = this.props.product;
+
+		const content = (
+			<div className="card-popover">
+				<b className="card-title"><p>{name}</p></b>
+				<p className="card-date">Lanzamiento: {date}</p>
+				<Carousel autoplay>
+					{/* <div>Hola</div> */}
+					{/* <div>Adios</div> */}
+					<img src={this.getPhoto(`${id}-1`)}/>
+					<img src={this.getPhoto(`${id}-2`)}/>
+					<img src={this.getPhoto(`${id}-3`)}/>
+					<img src={this.getPhoto(`${id}-4`)}/>
+				</Carousel>
+				<p style={{paddingTop: '1.5rem'}}>{description}</p>
+				{/* {this.getPhoto(`${id}-1`) ? <h1>Si</h1> : <h1>No</h1>} */}
+			</div>
+		);
 
 		return(
 			<div style={{ cursor: 'pointer', margin: 20 }}>
@@ -38,23 +55,26 @@ export default class ProductCard extends React.Component{
 						<img src={this.getPhoto(id)} style={{width: "100%", height: "107px"}}></img>
 						<Meta title={brand} description={price}></Meta>
 					</Card.Grid> */}
-					
+					<Popover content={content} placement="rightTop" overlayStyle={{ width: "15%" }}>
+
 					<div className="card-container">
 						<img src={this.getPhoto(id)} style={{width: "100%"}}></img>
 						<div className="card-text-container">
 							<Row type="flex" className="card-text-row">
 								<Col xs={{span: 14}} lg={{span: 14}}>
-									<p>{name}</p>
+									<p className="card-name">{name}</p>
 								</Col>
 								<Col xs={{span: 2}} lg={{span: 2}}></Col>
 								<Col xs={{span: 8}} lg={{span: 8}}>
-									<div className="price">
-										<p>ARS$ {price}</p>
+									<div >
+										<p className="price">$ {price}</p>
 									</div>
 								</Col>
 							</Row>
 						</div>
 					</div>
+
+					</Popover>
 				</Link>
 			</div>
 		)
